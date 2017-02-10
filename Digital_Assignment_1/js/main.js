14ca4b0e8f3490efe8c4d23f3cf83ee5d09b5f16
cmaldon4 <charlimaldonado@MacBook-Pro.local>
2017-02-09 22:55:53 -0500

    Background & Character

--- Digital_Assignment_1/js/main.js
+++ Digital_Assignment_1/js/main.js
@@ -13,26 +13,21 @@ window.onload = function() {
     
     "use strict";
     
-  
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+    var game = new Phaser.Game( 550, 550, Phaser.AUTO, 'game', { preload: preload, create: create} );
+    
+    function preload() {
+        game.load.image( 'background', 'assets/Earth.png' );
+        game.load.image( 'cowboy', 'assets/cowboy.png' );
+    }
+    
+    
+    function create() {
+        // Create a sprite at the center of the screen using the 'logo' image.
+        var background = game.add.sprite( 0, 0, 'background' );
+        var sprite1 = game.add.sprite(0,0, 'cowboy' );
+        sprite1.alignIn(background, Phaser.BOTTOM_LEFT, -50, -200);
 
-function preload() {
+    }
+    
 
-    //  You can fill the preloader with as many assets as your game requires
-
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
-    game.load.image('background', 'assets/Earth');
-
-}
-
-function create() {
-
-    //  This creates a simple sprite that is using our loaded image and
-    //  displays it on-screen
-    game.add.sprite(0, 0, 'background');
-
-}
 };
